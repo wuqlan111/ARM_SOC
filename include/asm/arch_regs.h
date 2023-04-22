16ffc1f0510d4bc4ae77fec1aecaf20bf1a377f9
wuqlan111 <wuqlan111@163.com>
2023-04-22 23:24:20 +0800

    Modify  arch_regs.

--- include/asm/arch_regs.h
+++ include/asm/arch_regs.h
@@ -2,17 +2,17 @@
 #ifndef  ARMv7_ARCH_REGS
 #define  ARMv7_ARCH_REGS
 
+#include  <stdint.h>
 
 
+#define   read_xpsr(name)    {       \
+                    uint32_t val = 0;    \
+                    __asm__ volatile("");       \
+                    }
 
-
-
-
-
-
-
-
-
+#define   write_xpsr(name, value)  {       \
+                    __asm__ volatile("");       \
+                    }
 
 
 
