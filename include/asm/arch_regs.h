b359d1f23ee68bba4366c6bab8d8fd0bee84f5cc
wuqlan111 <wuqlan111@163.com>
2023-04-22 23:29:58 +0800

    Modify arch_regs.

--- include/asm/arch_regs.h
+++ include/asm/arch_regs.h
@@ -8,22 +8,19 @@
 #define   read_xpsr(name)    {       \
                     uint32_t val = 0;    \
                     __asm__ volatile("");       \
-                    }
+                }
 
-#define   write_xpsr(name, value)  {       \
-                    __asm__ volatile("");       \
-                    }
-
+#define   write_xpsr(name, value)   (__asm__ volatile(""))
+                    
 
 
-
-
-
-
-
-
-
-
+#define   update_xpsr(name,  value, mask)  {        \
+                    uint32_t  val  =  0;            \
+                    __asm__  volatile();        \
+                    val &= ~mask;           \
+                    val |=  ((mask) & (value));
+                    __asm__  volatile();        \
+                }
 
 
 
