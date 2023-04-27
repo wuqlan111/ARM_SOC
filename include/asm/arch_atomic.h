67593a8683f5280249d2c618ece277bb27e87b59
wuqlan111 <wuqlan111@163.com>
2023-04-27 22:39:24 +0800

    Modify  arch_atomic.

--- include/asm/arch_atomic.h
+++ include/asm/arch_atomic.h
@@ -2,12 +2,14 @@
 #ifndef   ASM_ARCH_ATOMIC_H
 #define   ASM_ARCH_ATOMIC_H
 
+#include  "irqflags.h"
 
 
-
-
-
-
+static inline uint32_t atomic_read32(uint32_t * ptr)
+{
+    uint32_t  val = 0;
+    __asm__ volatile("")
+}
 
 
 
