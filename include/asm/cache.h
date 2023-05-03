b93612fb2f1fc17cb6ea739c0f0a5668a51b3026
wuqlan111 <wuqlan111@163.com>
2023-05-03 22:34:17 +0800

    Modify  cache.

--- include/asm/cache.h
+++ include/asm/cache.h
@@ -6,10 +6,14 @@
 
 #include  "arch_regs.h"
 
+#define  CTYPE_NO_CACHE     0
+#define  CTYPE_INSTRUCTION_CACHE        (1u)
+#define  CTYPE_DATA_CACHE          (2u)
+#define  CTYPE_SEPARETE_CACHE      (3u)
+#define  CTYPE_UNIFIED_CACHE       (4u)
 
 
-
-
+#define get_cache_type(level)    ( (REG32_READ(ID_CLIDR_REG_ADDR) >> ((level) - 1)) & 0x7)
 
 
 
