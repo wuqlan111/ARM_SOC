3a9725810722e35114f80125f58b802ab4928c47
wuqlan111 <wuqlan111@163.com>
2023-05-06 22:19:03 +0800

    Modify  arch_feature.

--- include/asm/arch_feature.h
+++ include/asm/arch_feature.h
@@ -5,17 +5,23 @@
 
 #include  "arch_regs.h"
 
+#define  ID_PFR0_THUMB_FLAG     (0xF0U)
+#define  ID_PFR0_THUMB2_SUPPORT   (0x30U)
+
+
 static inline  uint32_t can_support_thumb2(void)
 {
-    return  (REG32_READ(ID_PFR0_REG_ADDR) & 0xf0) == 0xf0;
+    return  (REG32_READ(ID_PFR0_REG_ADDR) & ID_PFR0_THUMB_FLAG) == ID_PFR0_THUMB2_SUPPORT;
 
 }
 
 
-static inline  uint32_t can_support_thumb2(void)
-{
-    return  (REG32_READ(ID_PFR0_REG_ADDR) & 0xf0) == 0xf0;
+#define  ID_DFR0_DEBUG_FLAG    (0xF00000U)
+#define  ID_DFR0_DEBUG_MEMORY_MAPPED   (0x100000U)
 
+static inline  uint32_t can_support_memory_mapped_debug(void)
+{
+    return  (REG32_READ(ID_DFR0_REG_ADDR) & ID_DFR0_DEBUG_FLAG) == ID_DFR0_DEBUG_MEMORY_MAPPED;
 }
 
 
