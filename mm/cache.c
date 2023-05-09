9f4f804e5cef4c565c7bd4ecca8d92a3a6fcf109
wuqlan111 <wuqlan111@163.com>
2023-05-09 23:43:24 +0800

    Modify  cache.

--- mm/cache.c
+++ mm/cache.c
@@ -1,6 +1,7 @@
 
 #include  <stdint.h>
 #include  <memory.h>
+#include  <math.h>
 
 #include  "arch_regs.h"
 #include  "cache.h"
@@ -104,6 +105,109 @@ inline  uint32_t  get_level_of_coherency(void)
     return  (REG32_READ(ID_CCSIDR_REG_ADDR) >> 24)  & 0x7;
 }
 
+
+
+inline  void  invalidate_all_icache_to_pou(void)
+{
+    REG32_WRITE(CAHCE_ICIALLU_REG_ADDR,  0);
+}
+
+inline  void  invalidate_icache_by_mva(uint32_t addr)
+{
+    REG32_WRITE(CAHCE_ICIMVAU_REG_ADDR,  addr);
+}
+
+
+
+inline  void  invalidate_dcache_by_mva(uint32_t addr)
+{
+    REG32_WRITE(CAHCE_DCIMVAC_REG_ADDR,  addr);
+}
+
+void  invalidate_dcache_by_set(uint32_t level, uint32_t set, uint32_t way)
+{
+    cache_info_t info = {0};    
+    if((level  > 7 ) || get_cache_info(level, &info))
+        return;
+
+    uint32_t  associaty  =  log2(info.cache_associativity);
+    uint32_t  line       =  log2(info.line_size);
+    uint16_t  sets       =  log2(info.set_number);
+
+    if ( (set > info.set_number) || (way > info.cache_associativity) )
+        return;
+
+    uint32_t way_flag  =  associaty? 0: (way << (32 - associaty));
+    uint32_t set_flag  =  line > 4 ? (set << line):set << 4;
+
+    uint32_t  val  =  way_flag | set_flag  | ((level -1) << 1);
+
+    REG32_WRITE(CAHCE_DCISW_REG_ADDR, val);
+
+}
+
+inline  void  clean_dcache_by_mva_pou(uint32_t addr)
+{
+    REG32_WRITE(CAHCE_DCCMVAU_REG_ADDR,  addr);
+}
+
+
+inline  void  clean_dcache_by_mva_poc(uint32_t addr)
+{
+    REG32_WRITE(CAHCE_DCCMVAC_REG_ADDR,  addr);
+}
+
+
+void  clean_dcache_by_set(uint32_t level, uint32_t set, uint32_t way)
+{
+    cache_info_t info = {0};    
+    if((level  > 7 ) || get_cache_info(level, &info))
+        return;
+
+    uint32_t  associaty  =  log2(info.cache_associativity);
+    uint32_t  line       =  log2(info.line_size);
+    uint16_t  sets       =  log2(info.set_number);
+
+    if ( (set > info.set_number) || (way > info.cache_associativity) )
+        return;
+
+    uint32_t way_flag  =  associaty? 0: (way << (32 - associaty));
+    uint32_t set_flag  =  line > 4 ? (set << line):set << 4;
+
+    uint32_t  val  =  way_flag | set_flag  | ((level -1) << 1);
+
+    REG32_WRITE(CAHCE_DCCSW_REG_ADDR, val);
+
+}
+
+
+inline  void  clean_invalidate_dcache_by_mva_poc(uint32_t addr)
+{
+    REG32_WRITE(CAHCE_DCCIMVAC_REG_ADDR,  addr);
+}
+
+
+void  clean_invalidate_dcache_by_set(uint32_t level, uint32_t set, uint32_t way)
+{
+    cache_info_t info = {0};    
+    if((level  > 7 ) || get_cache_info(level, &info))
+        return;
+
+    uint32_t  associaty  =  log2(info.cache_associativity);
+    uint32_t  line       =  log2(info.line_size);
+    uint16_t  sets       =  log2(info.set_number);
+
+    if ( (set > info.set_number) || (way > info.cache_associativity) )
+        return;
+
+    uint32_t way_flag  =  associaty? 0: (way << (32 - associaty));
+    uint32_t set_flag  =  line > 4 ? (set << line):set << 4;
+
+    uint32_t  val  =  way_flag | set_flag  | ((level -1) << 1);
+
+    REG32_WRITE(CAHCE_DCCISW_REG_ADDR, val);
+
+}
 
 
 
