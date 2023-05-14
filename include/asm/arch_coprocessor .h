8b17ef13e5c95c897891c4b3daf9b41bc93a9a4f
wuqlan111 <wuqlan111@163.com>
2023-05-14 19:38:58 +0800

    Modify  arch_coprocessor.

--- include/asm/arch_coprocessor .h
+++ include/asm/arch_coprocessor .h
@@ -13,6 +13,7 @@ enum {
     CP_ACCESS_PRIVILEGED,
     CP_ACCESS_RESERVED,
     CP_ACCESS_FULL,
+    CP_MAX_ACCESS = CP_ACCESS_FULL,
 };
 
 
@@ -26,12 +27,20 @@ static inline uint32_t is_cp_implemented(uint32_t cp)
     return  ret;
 }
 
-static  inline enable_or_disable_cp(uint32_t  cp)
+static  inline void set_corprocessor_access(uint32_t  cp, uint32_t access)
 {
+    if ( (access > CP_MAX_ACCESS) || cp >= MAX_CP_NUMBER)
+        return;
 
-
+    uint32_t  flag  =  access  << ( 2 * cp);
+    uint32_t  mask  =  0x3  << (2 * cp);
+    REG32_UPDATE(SCB_CPACR_REG_ADDR,  flag,  mask);
 
 }
+
+#define  DISABLE_CP(cp)  set_corprocessor_access((cp), CP_ACCESS_DENIED)
+#define  ENABLE_CP(cp)  set_corprocessor_access((cp), CP_ACCESS_FULL)
+
 
 
 
