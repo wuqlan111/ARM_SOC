e2c2b48015dd1f0ff5f9bca547d65ce2ab99aca9
wuqlan111 <wuqlan111@163.com>
2023-05-15 22:57:07 +0800

    Modify  traps.

--- kernel/traps.c
+++ kernel/traps.c
@@ -4,15 +4,55 @@
 #include  <string.h>
 
 #include "arch_regs.h"
+#include "asm/context_stack.h"
+
+
+void  do_reset()
+{
+
+
+}
+
+
+void  do_NMI()
+{
 
 
 
+}
 
 
+void  do_bus_fault(context_no_fp_regs_t regs)
+{
+    
+
+}
 
 
+void  do_usage_fault()
+{
 
 
+}
+
+
+void  do_svcall()
+{
+
+
+}
+
+void  do_debug()
+{
+
+
+}
+
+void  do_pendsv()
+{
+
+
+}
 
 
 
