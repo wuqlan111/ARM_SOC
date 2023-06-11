57bb7c9d46185faf49de5303aa8b6c563dc2565b
wuqlan111 <wuqlan111@163.com>
2023-06-11 23:24:29 +0800

    Modify  math_utils.

--- include/asm/math_utils.h
+++ include/asm/math_utils.h
@@ -5,7 +5,7 @@
 #include  <stdint.h>
 
 
-static  uint32_t math_log2(uint32_t value)
+static  uint32_t math_clog2(uint32_t value)
 {
     uint32_t  ret  =  0;
     for (int32_t i =  0; i < 31; i++) {
