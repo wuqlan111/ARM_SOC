260413438272463aabc4b73e847bf4ee47cdd1a1
wuqlan111 <wuqlan111@163.com>
2023-06-24 19:41:31 +0800

    Modify  mpu_test.

--- test/mpu_test.c
+++ test/mpu_test.c
@@ -8,6 +8,25 @@
 #include  "arch_printk.h"
 
 
+static  void  printf_mpu_region_config(mpu_region_config_t * config)
+{
+    if (!config) {
+        return;
+    }
+
+    __DBG_PRINTF_ALL("addr:\t0x%08x\n", config->base_addr);
+    __DBG_PRINTF_ALL("size:\t%d\n", config->size);
+    __DBG_PRINTF_ALL("sub_bits:\t0x%02x\n", config->sub_bits);
+    __DBG_PRINTF_ALL("access:\t%d\t", config->access);
+    __DBG_PRINTF_ALL("tex:\t%d\n", config->tex);
+    __DBG_PRINTF_ALL("shareable:\t%d\n", config->shareable);
+    __DBG_PRINTF_ALL("cacheable:\t%d\n", config->cacheable);
+    __DBG_PRINTF_ALL("write_back:\t%d\n", config->write_back);
+    __DBG_PRINTF_ALL("never_execute:\t%d\n", config->never_execute);
+
+}
+
+
 void  mpu_test(void)
 {
 
@@ -15,14 +34,27 @@ void  mpu_test(void)
     __DBG_PRINTF_ALL("mpu support max regions:\t%d\n", max_regions);
     /*get */
 
-    
+    mpu_region_config_t  region_conf =  {0};
+    region_conf.access   =  0x2;
+    region_conf.base_addr      =     0xf100;
+    region_conf.cacheable      =     1;
+    region_conf.tex            =     0x3;
+    region_conf.sub_bits       =     0x2f;
+    region_conf.size           =     64;
 
+    if (set_mpu_region_config(1,  &region_conf)) {
+        __DBG_PRINTF_ALL("set mpu region failed!\n");
+    } else {
+        printf_mpu_region_config(&region_conf);
+    }
 
+    memset(&region_conf,   0,  sizeof(mpu_region_config_t));
 
-
-
-
-
+    if (get_mpu_region_config(1,  &region_conf)) {
+        __DBG_PRINTF_ALL("get mpu region failed!\n");
+    } else {
+        printf_mpu_region_config(&region_conf);
+    }
 
 }
 
