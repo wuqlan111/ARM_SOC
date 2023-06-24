33f80ee33b3b99fdf8c1295c963cbebdd3cd515d
wuqlan111 <wuqlan111@163.com>
2023-06-24 19:04:55 +0800

    Modify  math_util.

--- lib/include/math_util.h
+++ lib/include/math_util.h
@@ -5,19 +5,18 @@
 #include  <stdint.h>
 
 
-static  inline  uint32_t  is_clog2(uint32_t  value)
+static  inline  uint32_t  is_powers_of_2(uint32_t  value)
 {
     return   !((value - 1) & value);
 }
 
 
 
-
-
-
-
-
-
+int32_t  get_low_bit_zero(uint32_t value,  uint32_t * bit_pos);
+int32_t  get_low_bit_no_zero(uint32_t value, uint32_t * bit_pos);
+int32_t  get_high_bit_zero(uint32_t value,  uint32_t * bit_pos);
+int32_t  get_high_bit_no_zero(uint32_t value, uint32_t * bit_pos);
+int32_t  math_clog2(uint32_t value,  uint32_t * log2);
 
 
 
