3ac340684fc7b528ad7e50882224b41a00d79917
wuqlan111 <wuqlan111@163.com>
2023-07-07 01:14:34 +0800

    Modify  mdk.

--- test/main.c
+++ test/main.c
@@ -12,8 +12,10 @@ extern  int32_t  svcall_test(uint32_t  param1,  uint32_t param2,
 int  main(void)
 {
 
-    __asm__ volatile("svc #3");
-    test_mm_fault();
+    int32_t  ret  =  0;
+    ret = svcall_test(12,1,123,1234);
+
+    __DBG_PRINTF_ALL("svcall ret: %d\n",  ret);
     while(1) ;
 
     return  0;
